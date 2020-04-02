99053800ff6179762c0bc09e7b0b0aba14bb59bf
LazarJovic <lazar.13jovic@gmail.com>
2020-04-02 20:20:07 +0200

    Create components
    Created components:
    - CreateCertificateComponent
    - CreateSubjectComponent
    - CertificateOverviewComponent

--- Frontend/pki-app/src/app/app-routing.module.ts
+++ Frontend/pki-app/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { CreateCertificateComponent } from './create-forms/create-certificate/create-certificate.component';
+import { CertificateOverviewComponent } from './certificate-overview/certificate-overview/certificate-overview.component';
 
 
 const routes: Routes = [
@@ -12,6 +14,15 @@ const routes: Routes = [
 {
   path: 'dashboard',
   component: DashboardComponent
+},
+{
+  path: 'create-certificate',
+  component: CreateCertificateComponent
+}
+,
+{
+  path: 'certificate-overview',
+  component: CertificateOverviewComponent
 }
 ];
 

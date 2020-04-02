8a19bda5070fe7e4ed7e1ed0af5a2fd3347189d9
LazarJovic <lazar.13jovic@gmail.com>
2020-04-02 20:21:05 +0200

    Revert "Revert "Create components""
    This reverts commit 1e01257b62ccad1f2fb532a120526fe5461f3a70.

--- Frontend/pki-app/src/app/app.module.ts
+++ Frontend/pki-app/src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatIconModule } from '@angular/material';
+import { CreateCertificateComponent } from './create-forms/create-certificate/create-certificate.component';
+import { CreateSubjectComponent } from './create-forms/create-subject/create-subject.component';
+import { CertificateOverviewComponent } from './certificate-overview/certificate-overview/certificate-overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    CreateCertificateComponent,
+    CreateSubjectComponent,
+    CertificateOverviewComponent
   ],
   imports: [
     BrowserModule,

9cfc0715dadf26af9db1889dec5e9bed855f6d18
LazarJovic <lazar.13jovic@gmail.com>
2020-04-03 17:04:36 +0200

    Create form for adding certificates
    Created form for adding certificates with basic certificate data fields.

--- Frontend/pki-app/src/app/create-forms/create-certificate/create-certificate.component.ts
+++ Frontend/pki-app/src/app/create-forms/create-certificate/create-certificate.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'create-certificate',
@@ -10,15 +11,32 @@ export class CreateCertificateComponent implements OnInit {
 
   createCertificateForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.createCertificateForm = new FormGroup({
-      'issuer': new FormControl(null),
-      'serialNumber': new FormControl(null),
-      'startDate': new FormControl(null),
-      'endDate': new FormControl(null)
+      'issuer': new FormControl(null, [Validators.required]),
+      'serialNumber': new FormControl(null, [Validators.required]),
+      'startDate': new FormControl({value: null}, [Validators.required]),
+      'endDate': new FormControl({value: null}, [Validators.required]),
+      'subject': new FormControl({value: null}, [Validators.required]),
+      'signatureAlgorithm': new FormControl({value: null}, [Validators.required]),
+      'pubKeyAlgorithm': new FormControl({value: null}, [Validators.required])
     });
+  }
+
+  chooseCert() {
+
+  }
+
+  createSubject() {
+
+  }
+
+  back() {
+    this.router.navigate(['']);
   }
 
 }
